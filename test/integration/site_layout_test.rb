23c06c4c0380d5f0f4c8ed62f36bf18e64049344
shun <samesamejinbei@gmail.com>
2024-12-10 20:10:40 +0900

    Add user following

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -49,6 +49,11 @@ test "layout links when logged in user" do
   end
     
 
-
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
   
 end
